d0be059d2fa99578f17f9ab2559ead09ba2cf6c0
ali mousavi <alimusavi67@gmail.com>
2024-06-22 11:16:03 +0330

    fix: dangerouslySetInnerHTML sanitize

--- src/components/Episode/EpisodeItem.tsx
+++ src/components/Episode/EpisodeItem.tsx
@@ -6,6 +6,7 @@ import moment from 'moment';
 import Skeleton from '../Basics/Skeleton/Skeleton';
 import useImageLoader from '../../hooks/useImageLoader';
 import DefaultImage from '../Basics/DefaultImage/DefaultImage';
+import DOMPurify from 'dompurify';
 
 interface EpisodeItemProps {
   episode: Episode;
@@ -17,7 +18,7 @@ const EpisodeItem: React.FC<EpisodeItemProps> = ({ episode }) => {
     ? moment(episode.airstamp).tz(timeZone.timeZone).format('YYYY/MM/DD HH:mm')
     : 'No date available';
   const isLoading = useImageLoader(episode.image?.medium);
-
+  const sanitizeSummary = episode?.summary ? DOMPurify.sanitize(episode?.summary) : '<em>No summary available</em>';
   return (
     <li key={episode.id} className="p-2 border rounded-lg">
       <div className="relative group overflow-hidden">
@@ -26,10 +27,7 @@ const EpisodeItem: React.FC<EpisodeItemProps> = ({ episode }) => {
       <div className="mt-2">
         <h2 className="text-lg font-semibold">{episode.name}</h2>
         <p className="text-sm text-gray-600">{formattedDate}</p>
-        <p
-          className="text-sm mt-2"
-          dangerouslySetInnerHTML={{ __html: episode.summary || '<em>No summary available</em>' }}
-        ></p>
+        <p className="text-sm mt-2" dangerouslySetInnerHTML={{ __html: sanitizeSummary }}></p>
       </div>
     </li>
   );
